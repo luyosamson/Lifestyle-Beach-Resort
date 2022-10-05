e6d5931418ca0448eb5fac111060179d790748df
luyosamson <luyosamson@gmail.com>
2022-10-05 21:56:20 +0300

    Initial commit

---
+++ src/pages/Rooms.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Rooms(){
+
+
+return<div>Hello from Rooms page</div>
+
+
+}
+export default Rooms;
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,13 @@
+//Creatin  a class based component for single rooms page
+
+import React,{Component} from "react";
+
+export default class SingleRoom extends Component{
+
+
+    render(){
+
+        return <div>Hello from singel room</div>
+    }
+
+}
